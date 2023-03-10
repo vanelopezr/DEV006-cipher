0d23a233838045b84c5366691da0c205394fe963
vanelopezr <vanne.lopez.r@gmail.com>
2023-03-10 10:41:00 -0300

    basico

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import cipher from './cipher.js';
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K",
+"L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
-console.log(cipher);
+
+
+
+//import cipher from './cipher.js';
+// console.log(cipher);
