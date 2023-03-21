ebd000f61508463b7aadcca5aa2f980c2ad5b0b3
vanelopezr <vanne.lopez.r@gmail.com>
2023-03-21 13:20:55 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,41 @@
+const cipher = {
+
+  encode: (offset, string) => {
+    //obtener los valores
+    let result = "";
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw  TypeError ("Ingrese caracteres válidos"); }
+    //iterar en cada letra y obtener el número ascii
+    for (let i = 0; i < string.length; i++) {
+      const asciiPosition = string[i].charCodeAt();
+      if (asciiPosition >= 65 && asciiPosition <= 90) {
+        //aplicar la formula
+        const asciiTxt = (((asciiPosition - 65 + offset) % 26) + 65);
+        result += String.fromCharCode(asciiTxt);
+        //agregar los espacios
+      } else if (asciiPosition === 32) { 
+        result += " ";
+      }
+    }
+    return result;
+  },
+
+  decode: (offset, string) => {
+    let result = "";
+    if (typeof offset !== "number" || typeof string !== "string") {
+      throw  TypeError ("Ingrese caracteres válidos"); }
+    for (let i = 0; i < string.length; i++) {
+      const asciiPosition = string[i].charCodeAt();
+      if (asciiPosition >= 65 && asciiPosition <= 90) {
+        result += String.fromCharCode((((asciiPosition - 65 - offset + 26)+26) % 26) + 65);
+      } else if (asciiPosition === 32) { 
+        result += " ";    
+      } else {
+        result += string[i];
+      }
+    }
+    return result;
+  }
+
+}
+export default cipher;
---
+++ index.js
@@ -0,0 +1,32 @@
+import cipher from './cipher.js';
+//console.log(cipher);
+//llamar los elementos 
+const offset = document.getElementById ("offset");
+const string = document.getElementById ("string");
+const result = document.getElementById ("messageResult");
+
+//poner en mayúsculas el texto
+string.addEventListener("input", function() {
+  const text = string.value.toUpperCase();
+  string.value = text;
+});
+//ligar la funcion a mis botones
+const encodeButton = document.getElementById ("encodeButton");
+encodeButton.addEventListener("click",function () {
+
+  result.innerHTML = cipher.encode(parseInt(offset.value),string.value);
+  
+})
+
+const decodeButton = document.getElementById ("decodeButton");
+decodeButton.addEventListener ("click", function(){
+
+  result.innerHTML = cipher.decode (parseInt(offset.value), string.value);
+})
+
+//recargar el sitio 
+const goBackButton = document.getElementById("goBack")
+goBackButton.addEventListener("click",()=>{
+  history.go(0);
+});
+
